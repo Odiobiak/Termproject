989be2004881213c32462b391f78114c57ce3f1f
Odiche Obiakarije <odicheobiakarije@gmail.com>
2020-04-13 05:37:25 -0400

    Update app.py

--- Docker-files/Frontend/app.py
+++ Docker-files/Frontend/app.py
@@ -10,8 +10,7 @@ from google.cloud import storage
 app = Flask(__name__)
 
 '''Storage Client'''
-storage_client = storage.Client.from_service_account_json(
-    'termproject-273720-0d7405cf24f6.json')
+storage_client = storage.Client(project="termproject-273720")
 
 #job_client
 job_client = dataproc_v1.JobControllerClient(client_options={
@@ -55,8 +54,25 @@ def home():
     # print(downloaded_blob)
     #content = ''
     if request.method == 'POST':
-        #result = request.form
+        
+        #hugo file is submitted
         if request.form.get('Hugo.tar.gz') == 'Hugo.tar.gz':
+            #Creating a job client
+            job1 = {
+                'reference' : {
+                    'job_id' : 'jobhugo'
+                },
+                'placement' : {
+                    'cluster_name' : 'cluster-2',
+                    #'clusterUuid' : '3d4db54c-3b47-4724-ae02-90b4b825d6df'
+                },
+                'hadoop_job' : {
+                    'main_class' : 'InvertedIndex',
+                    'args' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/Data/Hugo.tar.gz', 'gs://dataproc-staging-us-west1-494286768598-sn2scqkt/hugo_out'],
+                    'jar_file_uris' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/JAR/invertedindex.jar']
+                }
+            }
+            job_response = job_client.submit_job(project_id, region, job1)
             blob = buckets.get_blob('finalhugo.txt')
             result = blob.download_as_string()
             with open("hugofile.txt", "wb") as fl:
@@ -64,12 +80,25 @@ def home():
             with open('hugofile.txt', 'r') as f:
                 #lines = f.readline().splitlines()
                 content = f.readlines()
-                #for line in content:
-                    #content = content.splitlines()
-                    #print (line)
             return render_template('hugo.html', content=content)
 
         elif request.form.get('shakespeare.tar.gz') == 'shakespeare.tar.gz':
+                  #Creating a job client
+            job2 = {
+                'reference' : {
+                    'job_id' : 'jobshakes'
+                },
+                'placement' : {
+                    'cluster_name' : 'cluster-2',
+                    #'clusterUuid' : '3d4db54c-3b47-4724-ae02-90b4b825d6df'
+                },
+                'hadoop_job' : {
+                    'main_class' : 'InvertedIndex',
+                    'args' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/Data/shakespeare.tar.gz', 'gs://dataproc-staging-us-west1-494286768598-sn2scqkt/shakespeare_out'],
+                    'jar_file_uris' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/JAR/invertedindex.jar']
+                }
+            }
+            job_response2 = job_client.submit_job(project_id, region, job2)
             with open("shakespearfile.txt", "wb") as f1:
                 f1.write(result2)
             with open('shakespearfile.txt', 'r') as f1:
@@ -78,6 +107,22 @@ def home():
             return render_template('shakes.html', content=content)
 
         elif request.form.get('Tolstoy.tar.gz') == 'Tolstoy.tar.gz':
+                  #Creating a job client
+            job3 = {
+                'reference' : {
+                    'job_id' : 'jobtol'
+                },
+                'placement' : {
+                    'cluster_name' : 'cluster-2',
+                    #'clusterUuid' : '3d4db54c-3b47-4724-ae02-90b4b825d6df'
+                },
+                'hadoop_job' : {
+                    'main_class' : 'InvertedIndex',
+                    'args' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/Data/tolstoy.tar.gz', 'gs://dataproc-staging-us-west1-494286768598-sn2scqkt/tolstoy_out'],
+                    'jar_file_uris' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/JAR/invertedindex.jar']
+                }
+            }
+            job_response3 = job_client.submit_job(project_id, region, job3)
             with open("Tolstoyfile.txt", "wb") as f2:
                 f2.write(result3)
             with open('Tolstoyfile.txt', 'r') as f2:
@@ -86,6 +131,22 @@ def home():
             return render_template('Tol.html', content=content)
 
         elif request.form.get('all') == 'all':
+                  #Creating a job client
+            job4 = {
+                'reference' : {
+                    'job_id' : 'joball'
+                },
+                'placement' : {
+                    'cluster_name' : 'cluster-2',
+                    #'clusterUuid' : '3d4db54c-3b47-4724-ae02-90b4b825d6df'
+                },
+                'hadoop_job' : {
+                    'main_class' : 'InvertedIndex',
+                    'args' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/Data', 'gs://dataproc-staging-us-west1-494286768598-sn2scqkt/all_out'],
+                    'jar_file_uris' : ['gs://dataproc-staging-us-west1-494286768598-sn2scqkt/JAR/invertedindex.jar']
+                }
+            }
+            job_response4 = job_client.submit_job(project_id, region, job4)
             with open("all.txt", "wb") as f3:
                 f3.write(result4)
             with open('all.txt', 'r') as f3:
